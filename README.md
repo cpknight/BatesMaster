fc7c8f2df7595fa5112cedb6b8c6b45695fff592
CP Knight <chris@netcog.net>
2016-10-20 15:21:54 -0600

    Reorganization and Notes commit
    Updated script for simplicity and performance. Added notes. Removed
    extra files.

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-batespdf
-========
+------------------------------------------------------------------------------
+*batespdf* - *PDF Bates Stamper* in Python3
+------------------------------------------------------------------------------
 
-PDF Bates Stamper
+  Credit for this script is due primarily to:
+                     `https://github.com/adlukasiak/BatesMaster/`
 
-http://www.reportlab.com/docs/reportlab-reference.pdf
+  PDF library reference can be found in: 
+                     `http://www.reportlab.com/docs/reportlab-reference.pdf`
 
+  Usage:
+  
+    ```
+    $ ./batespdf.py
+      Usage: ./batespdf.py [input PDF] [prefix string]
+         eg. ./batespdf.py DOC00001.pdf DOC00001
+             Notes: - both arguments are mandatory.
+                    - output filename has "-bates-numbered.pdf" appended.
+    ```
 
-
+------------------------------------------------------------------------------
+(c) Copyright 2016 Christopher P. Knight                                   eof
--- batespdf.py
+++ batespdf.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python3
 
+import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from io import BytesIO
 
-orig_pdf = PdfFileReader(open("test.pdf", "rb"))
-# print "document1.pdf has %d pages." % input1.getNumPages()
+if (len(sys.argv) < 3):
+  print("Usage: " + sys.argv[0] + " [input PDF] [prefix string]")
+  print("   eg. " + sys.argv[0] + " DOC00001.pdf DOC00001")
+  print("       Notes: - both arguments are mandatory.")
+  print("              - output file has \"-bates-numbered.pdf\" appended to the filename.")
+  sys.exit(1)
+
+orig_pdf = PdfFileReader(open(sys.argv[1], "rb"))
 num_pages = orig_pdf.getNumPages()
 page_size = orig_pdf.getPage(0).mediaBox
-
-### inputs ####
-#font_size = 10
-#prefix = "BB"
-#starting_number_str = "00067"
-# mode = "right" #"left" and "center"
-
-input_config = {
-                # "top-l" : {"mode":"top-l" ,"font_size":12,"starting_num":"3","prefix":"TL-"},
-                "top-r" : {"mode":"top-r" ,"font_size":10,"starting_num":"000","prefix":"FILENAME."},
-                # "left"  : {"mode":"left"  ,"font_size":12,"starting_num":"1","prefix":"LEFT-"},
-                # "center": {"mode":"center","font_size":10,"starting_num":"2","prefix":"CENTER-"},
-                # "right" : {"mode":"right" ,"font_size":12,"starting_num":"3","prefix":"RIGHT-"},
-                }
-#input_config = {
-#                "left"  : {"mode":"left"  ,"font_size":10,"starting_num":""     ,"prefix":"CONFIDENTIAL - ATTORNEYS EYES ONLY"},
-#                "center": {"mode":"center","font_size":10,"starting_num":""     ,"prefix":""},
-#                "right" : {"mode":"right" ,"font_size":10,"starting_num":"00067","prefix":""},
-#                }
-
-### and figure few things out based on inputs ###
 height = float(page_size[3]/72)*inch
 width = float(page_size[2]/72)*inch
 margin = 0.1*inch
@@ -47,30 +33,16 @@ packet = BytesIO()
 canv = canvas.Canvas(packet,pagesize=(width,height))
 
 for i in range(0,num_pages):
-    for j in input_config:
-        
-        #figure out few things about the starting number
-        num_length = len(str(input_config[j]["starting_num"]))
-#        page_number_offset = int(input_config[j]["starting_num"] if input_config[j]["starting_num"] != "" else -1)
-#        page_number_offset = int(input_config[j]["starting_num"])
-
-        canv.setFont("Courier", input_config[j]["font_size"]) #need to reset font for each page
-        # canv.setStrokeColorRGB(1,0,0)
-        canv.setFillColorRGB(200,0,0)
-            
-#        bates = str(canv.getPageNumber()+page_number_offset).zfill(num_length) if page_number_offset > 0 else ""
-        bates = str(canv.getPageNumber()).zfill(3) 
-        
-        text = input_config[j]["prefix"] + bates
-        mode = input_config[j]["mode"]
-        # print canv.getPageNumber(),text, width, config[mode]["x"]
-    
-        config[mode]["func"](config[mode]["x"],config[mode]["y"],text)
-    canv.showPage() #move to the next page
+  num_length = len("000")
+  canv.setFont("Courier", 10) #need to reset font for each page
+  canv.setFillColorRGB(200,0,0)
+  bates = str(canv.getPageNumber()).zfill(3) 
+  text = sys.argv[2] + bates
+  mode = "top-r"
+  config[mode]["func"](config[mode]["x"],config[mode]["y"],text)
+  canv.showPage() 
 canv.save()
 
-# Time to merge
-#move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
@@ -81,6 +53,6 @@ for i in range(0,num_pages):
     page.mergePage(new_pdf.getPage(i))
     output.addPage(page)
 # finally, write "output" to a real file
-outputStream = open("test_bates.pdf", "wb")
+outputStream = open((sys.argv[1] + "-bates-numbered.pdf"), "wb")
 output.write(outputStream)
 outputStream.close()
