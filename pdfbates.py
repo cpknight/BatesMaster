e21cb37766839fcb32ecbcef62bb8ad7dd3e26df
CP Knight <chris@netcog.net>
2016-10-21 08:46:08 -0600

    Updated Code Organization, Licence
    Update organization of code and added BSD 2-clause licence.

--- pdfbates.py
+++ pdfbates.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+# ----------------------------------------------------------------------------
+# pdfbates.py - PDF Bates Stamper for python3
+# ----------------------------------------------------------------------------
+# Notes: - This code is subject to a BSD 2-clause licence that appears below.
+#        - Credit for original fork to https://github.com/adlukasiak and
+#          https://github.com/adlukasiak/BatesMaster 
+# ----------------------------------------------------------------------------
 
 import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
@@ -14,46 +21,72 @@ if (len(sys.argv) < 3):
   print("              - output file has \"-bates-numbered.pdf\" appended to the filename.")
   sys.exit(1)
 
-orig_pdf = PdfFileReader(open(sys.argv[1], "rb"))
-num_pages = orig_pdf.getNumPages()
-page_size = orig_pdf.getPage(0).mediaBox
-height = float(page_size[3]/72)*inch
-width = float(page_size[2]/72)*inch
-margin = 0.1*inch
+originalPDF    = PdfFileReader(open(sys.argv[1], "rb"))
+numberOfPages  = originalPDF.getNumPages()
+pageDimensions = originalPDF.getPage(0).mediaBox
+pageHeight     = float(pageDimensions[3]/72)*inch
+pageWidth      = float(pageDimensions[2]/72)*inch
+pageMargin     = 0.1*inch
 
-config = {
-          "top-l" :{"x":margin        ,"y":height-(margin*2), "func": lambda x,y,text : canv.drawString(x,y,text)},
-          "top-r" :{"x":width-margin  ,"y":height-(margin*2), "func": lambda x,y,text : canv.drawRightString(x,y,text)},
-          "right" :{"x":width-margin  ,"y":margin, "func": lambda x,y,text : canv.drawRightString(x,y,text)},
-          "left"  :{"x":margin        ,"y":margin, "func": lambda x,y,text : canv.drawString(x,y,text)},
-          "center":{"x":width/2       ,"y":margin, "func": lambda x,y,text : canv.drawCentredString(x,y,text)}
-          }
+stampPositionsOnPage = {
+    "top-left"     :{"x":pageMargin            ,"y":pageHeight-(pageMargin*2),  "lambdaFunction": lambda x,y,stampString : pageCanvas.drawString(x,y,stampString)        },
+    "top-right"    :{"x":pageWidth-pageMargin  ,"y":pageHeight-(pageMargin*2),  "lambdaFunction": lambda x,y,stampString : pageCanvas.drawRightString(x,y,stampString)   },
+    "bottom-right" :{"x":pageWidth-pageMargin  ,"y":pageMargin,                 "lambdaFunction": lambda x,y,stampString : pageCanvas.drawRightString(x,y,stampString)   },
+    "bottom-left"  :{"x":pageMargin            ,"y":pageMargin,                 "lambdaFunction": lambda x,y,stampString : pageCanvas.drawString(x,y,stampString)        },
+    "bottom-center":{"x":pageWidth/2           ,"y":pageMargin,                 "lambdaFunction": lambda x,y,stampString : pageCanvas.drawCentredString(x,y,stampString) }
+  }
 
-# the temp file should be same size as the original document
-packet = BytesIO()
-canv = canvas.Canvas(packet,pagesize=(width,height))
+pageData = BytesIO()
+pageCanvas = canvas.Canvas(pageData,pagesize=(pageWidth,pageHeight))
 
-for i in range(0,num_pages):
-  num_length = len("000")
-  canv.setFont("Courier", 10) #need to reset font for each page
-  canv.setFillColorRGB(200,0,0)
-  bates = str(canv.getPageNumber()).zfill(3) 
-  text = sys.argv[2] + bates
-  mode = "top-r"
-  config[mode]["func"](config[mode]["x"],config[mode]["y"],text)
-  canv.showPage() 
-canv.save()
+for i in range(0,numberOfPages):
+  numberLength = len("000")  # 3, but code retained for future tweaking
+  pageCanvas.setFont("Courier", 10) # need to reset font for each page
+  pageCanvas.setFillColorRGB(0.95,0.65,0.65)
+  batesNumber = str(pageCanvas.getPageNumber()).zfill(numberLength) 
+  stampString = sys.argv[2] + batesNumber
+  stampPosition = "top-right"
+  stampPositionsOnPage[stampPosition]["lambdaFunction"](stampPositionsOnPage[stampPosition]["x"],stampPositionsOnPage[stampPosition]["y"],stampString)
+  pageCanvas.showPage() 
 
-packet.seek(0)
-new_pdf = PdfFileReader(packet)
-# read your existing PDF
-output = PdfFileWriter()
+pageCanvas.save()
+pageData.seek(0)
+newPDF = PdfFileReader(pageData)
+outputData = PdfFileWriter()
+
 # add the "watermark" (which is the new pdf) on the existing page
-for i in range(0,num_pages):
-    page = orig_pdf.getPage(i)
-    page.mergePage(new_pdf.getPage(i))
-    output.addPage(page)
-# finally, write "output" to a real file
-outputStream = open((sys.argv[1] + "-bates-numbered.pdf"), "wb")
-output.write(outputStream)
-outputStream.close()
+for i in range(0,numberOfPages):
+  thisPage = originalPDF.getPage(i)
+  thisPage.mergePage(newPDF.getPage(i))
+  outputData.addPage(thisPage)
+
+outputDataStream = open((sys.argv[1] + "-bates-numbered.pdf"), "wb")
+outputData.write(outputDataStream)
+outputDataStream.close()
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016, Christopher P. Knight. All rights reserved.
+#
+# Redistribution  and  use  in  source  and  binary  forms,  with or  without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of  source  code must retain the above copyright notice, 
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions  in  binary  form  must  reproduce  the  above copyright 
+#    notice,  this list  of  conditions  and  the following disclaimer in the 
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND  ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE  DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE  FOR  ANY DIRECT,  INDIRECT,   INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR 
+# CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT  LIMITED  TO, PROCUREMENT  OF 
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS OF USE, DATA, OR PROFITS;  OR BUSINESS 
+# INTERRUPTION) HOWEVER  CAUSED  AND ON ANY  THEORY OF LIABILITY,  WHETHER IN 
+# CONTRACT,  STRICT  LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING  IN ANY WAY  OUT OF THE USE  OF THIS  SOFTWARE,  EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------------
+#                                                                          eof
